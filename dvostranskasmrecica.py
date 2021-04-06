8d0b8c07d371058cb55c8ea63f296bbc1e572031
kokicarvesolja <kristofer.cepon.povsic@gmail.com>
2021-04-06 17:21:31 +0200

    Učim se git commitat

---
+++ dvostranskasmrecica.py
@@ -0,0 +1,11 @@
+# Napiši program, ki od uporabnika dobi naravno število n, nato pa nariše "smrekico" iz zvezdic. 
+# Smrekica naj ima n vrstic. 
+
+n = int(input("Naravno število n: ")) +1
+i = 1
+desna_stran = 0
+
+for i in range(1, n):
+    n -=1
+    print(n*" ", i*"*" + desna_stran*"*")
+    desna_stran += 1
